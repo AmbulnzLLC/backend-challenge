bfcf7ea3c2b27dfdd64c8f21933d3ea359425863
Mirii H <miraharding@gmail.com>
2022-07-10 19:39:07 -0400

    wrote logic for basic get routes, working on post route for orders now

--- Routes/orders.js
+++ Routes/orders.js
@@ -2,31 +2,63 @@ const express = require("express");
 const router = new express.Router();
 const ExpressError = require("../expressError");
 import { PrismaClient } from '@prisma/client'
+const date = require('date-and-time');
+
+const now = new Date();
 
 const prisma = new PrismaClient()
 
-
+// gets all orders and returns a paginated array of order objects to be used by the DOM
 router.get("/", function(req, res, next) {
+    const results = await prisma.order.findMany({
+        skip: 3,
+        take: 2,
+      })
+    if (results.length <= 0) {
+        let error = new ExpressError('no results found', 204)
+        return next(error)
+    }
 
+  return res.json(results);
 });
 
 
 router.get("/:orderId", function(req, res, next) {
+    const { orderId } = req.params;
+
+    const result = await prisma.user.findUnique({
+        where: {
+          id: orderId,
+        },
+        include: {
+            items: true,
+        }
+      });
+
+      if(!result){
+        let error = new ExpressError('no result found', 204)
+        return next(error)
+    };
+
+   return res.json(result)
 
 });
 
 router.post("/newOrder", function(req, res, next) {
-//   const result = jsonschema.validate(req.body, bookSchema);
+    const {flavors, qty} = req.body;
 
-//   if (!result.valid) {
 
-//     let listOfErrors = result.errors.map(error => error.stack);
-//     let error = new ExpressError(listOfErrors, 400);
-//     return next(error);
-//   }
+   
+    
+    const newOrder = await prisma.orderItems.create({
+        data: {
+           
 
-//   const { book } = req.body;
-//   return res.json(book);
+        },
+    
+    })
+  
+    return res.json(newOrder)
 });
 
 
